f758a416b29fea69e0e0b2824582f7e17bf30f8d
Usuario-PC\Usuario <adri.espejo@gmail.com>
2017-10-10 13:59:14 +0200

    quitado mode de Game, arreglo bug del login/register y añadido parte del menu de crear/cargar partida y ver ranking

--- MasterMind/src/mastermind/MasterMind.java
+++ MasterMind/src/mastermind/MasterMind.java
@@ -24,21 +24,52 @@ public class MasterMind {
         while(!entrado){
             System.out.print("Escribe login para iniciar sesión, o register para registrarte." + "\n");
             String start = input.nextLine();
-            if(start == "login"){   //no entra nunca en ninguna de las 2 opciones
-                System.out.print("Introduce el nombre de usuario." + "\n");
-                System.out.print("Introduce la contraseña." + "\n");
+            if(start.equals("login")){
+                System.out.print("Introduce el nombre de usuario:" + "\n");
+                String usuario = input.nextLine();
+                System.out.print("Introduce la contraseña:" + "\n");
+                String contraseña = input.nextLine();
+                /*
+                Jugador jugador = new Jugador();
+                jugador.login(usuario,contraseña);
+                */
                 entrado = true;
             }
-            else if(start == "register"){
-                System.out.print("Introduce el nombre de usuario." + "\n");
-                System.out.print("Introduce la contraseña." + "\n");
+            else if(start.equals("register")){
+                System.out.print("Introduce el nombre de usuario:" + "\n");
+                String usuario = input.nextLine();
+                System.out.print("Introduce la contraseña:" + "\n");
+                String contraseña = input.nextLine();
+                /*
+                Jugador jugador = new Jugador();
+                jugador.register(usuario,contraseña);
+                */
                 entrado = true;
             }
             else System.out.print("Entrada no válida" + "\n");
         }
-           
-        Game game = new Game("1", "1", "1");           
-        
+        entrado = false;
+        while(!entrado){
+            System.out.print("Escribe crear para jugar una partida nueva, cargar para jugar una partida ya empezada o ranking para ver los records de puntuación." + "\n");
+            String jugar = input.nextLine();
+            if(jugar.equals("crear")){
+                System.out.print("Introduce el id para la nueva partida:" + "\n");
+                String idPartida = input.nextLine();
+                System.out.print("Introduce la dificultad (puede ser fácil, medio o difícil):" + "\n");
+                String dificultad = input.nextLine();
+                Game game = new Game(idPartida,dificultad);
+                entrado = true;
+            }
+            else if (jugar.equals("cargar")){
+                System.out.print("Partida guardadas:" + "\n");  //aqui habria un bolcado de los posibles ids
+                
+                System.out.print("Introduce el id de la partida guardada:" + "\n");
+                //comprobacion de que el id es posible, y si no vuelta a empezar
+                entrado = true;
+            }
+            else System.out.print("Entrada no válida" + "\n");
+        }
+        Game game = new Game("1", "1");           
     }
     
 }
