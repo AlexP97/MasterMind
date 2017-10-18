b91355ada1cc321481d19003510db6b6d9779523
Adrián Espejo <adri.espejo@gmail.com>
2017-10-18 17:05:42 +0200

    cambios en crear, jugar y cargar partida

--- MasterMind/src/mastermind/MasterMind.java
+++ MasterMind/src/mastermind/MasterMind.java
@@ -44,7 +44,7 @@ public class MasterMind {
                         System.out.print("Introduce la contraseña:" + "\n");
                         String contraseña = input.nextLine();
 
-                        Jugador jugador = new Jugador();
+                        jugador = new Jugador();
                         Boolean b = jugador.register(usuario,contraseña);
                         if(!b) estado = 0;
                     }
@@ -58,12 +58,18 @@ public class MasterMind {
                     System.out.print("Escribe crear para jugar una partida nueva, cargar para jugar una partida ya empezada, ranking para ver los records de puntuación o salir para volver al menú anterior." + "\n");
                     String jugar = input.nextLine();
                     if(jugar.equals("crear")){
+                        System.out.print("Escribe el id de la partida" + "\n");
+                        String id = input.nextLine();
+                        System.out.print("Escribe la dificultad de la partida" + "\n");
+                        String dif = input.nextLine();
+                        System.out.print("Escribe el modo de la partida (codebreaker o codemaker)" + "\n");
+                        String mod = input.nextLine();
                         Game game = new Game();
-                        game.juega();
+                        game.juega(jugador,id,dif,mod);
                     }
                     else if (jugar.equals("cargar")){
                         Game game = new Game();
-                        game.LoadGame(jugador.getName());
+                        game.LoadGame(jugador);
 //                      game.continua();
                     }
                     else if (jugar.equals("ranking")){
