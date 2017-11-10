fc56e6451a32fad55eac8a15a3b64aaf424ac03c
Alex <alejandro.perez.97@hotmail.com>
2017-11-10 23:29:36 +0100

    drivers acabaos

--- MasterMind/test/domain/CodeMakerDriver.java
+++ MasterMind/test/domain/CodeMakerDriver.java
@@ -5,22 +5,45 @@
  */
 package domain;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author usuario
  */
 public class CodeMakerDriver {
     CodeMaker codemaker;
+    CodePegStub codePeg;
+    JugadorStub jugador;
+    
+    public CodeMakerDriver(){
+        jugador = new JugadorStub();
+        codemaker = new CodeMaker(true,jugador.getNFichas(),jugador.getNColores());
+        codePeg = new CodePegStub();
+        testConstructor();
+        testDonaPatro();
+        testJugar();
+    }
     
     public void testConstructor() {
-        
+        boolean b = false;
+        int nf = 2;
+        int nc = 3;
+        CodeMaker cd = new CodeMaker(b, nf, nc);
     }
      
     public void testDonaPatro() {
-        
+        ArrayList<Integer> a = codemaker.dona_patro("IA");  
     }
     
     public void testJugar() {
-        
+        CodePeg cp = codePeg.create();
+        ArrayList<CodePeg> a = new ArrayList<>();
+        ArrayList<CodePeg> b = new ArrayList<>();
+        for(int i = 0; i < jugador.getNFichas(); i++) {
+            a.add(cp);
+            b.add(cp);
+        }
+        ArrayList<Integer> c = codemaker.jugar("IA", a, b);
     }
 }
--- MasterMind/test/domain/JugadorDriver.java
+++ MasterMind/test/domain/JugadorDriver.java
@@ -6,6 +6,7 @@
 package domain;
 
 import java.util.ArrayList;
+import utils.Pair;
 
 /**
  *
@@ -13,29 +14,52 @@ import java.util.ArrayList;
  */
 public class JugadorDriver {
     Jugador jugador;
+    CodePegStub codePeg;
+    
+    public JugadorDriver(){
+        jugador = new Jugador(4,5);
+        codePeg = new CodePegStub();
+        testConstructor();
+        testConstructor2();
+        testCreaArray();
+        testRegister();
+        testLogin();
+        testGetName();
+        testGetPassword();
+        getNColores();
+        getNFichas();
+        testSetIA();
+        testEsIA();
+        testDonaSolucio();
+    }
     
     public void testConstructor() {
-        
+        Jugador j = new Jugador();
     }
+    
+    public void testConstructor2() {
+        Jugador j = new Jugador(3,4);
+    }
+            
     
     public void testCreaArray(){
         ArrayList<Integer> a = jugador.creaArray(1,1,1,1);
     }
     
     public void testRegister() {
-        
+        Pair<Boolean, String> p = jugador.register("Juan", "456");
     }
     
     public void testLogin() {
-        
+        Pair<Boolean, String> p = jugador.login("Juan", "456");
     }
     
     public void testGetName() {
-        
+        String n = jugador.getName();
     }
     
     public void testGetPassword() {
-        
+        String c = jugador.getPassword();
     }
     
     public void getNColores() {
@@ -47,15 +71,22 @@ public class JugadorDriver {
     }
     
     public void testSetIA() {
-        
+        jugador.setIA();
     }
     
     public void testEsIA() {
-        
+        Boolean b = jugador.esIA();
     }
     
     public void testDonaSolucio() {
-        
+        CodePeg cp = codePeg.create();
+        ArrayList<CodePeg> a = new ArrayList<>();
+        ArrayList<CodePeg> b = new ArrayList<>();
+        for(int i = 0; i < jugador.getNFichas(); i++) {
+            a.add(cp);
+            b.add(cp);
+        }
+        ArrayList<Integer> c = jugador.donaSolucio(a, b);
     }
             
     
